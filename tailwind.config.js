00801a9a65fcbb02c7eb95fc1b1c1a7140c9f62a
Supun Liyanage <174965581+SupunLiyanage88@users.noreply.github.com>
2025-08-20 19:30:10 +0530

    init reactnative

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#37A77D", // Green
+        secondary: "#0B4057", // Dark 
+        
+      },
+    },
+  },
+  plugins: [],
+};
