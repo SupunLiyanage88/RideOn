80edbb97c913a089e0886ed0618eecd74405c40a
Supun Liyanage <174965581+SupunLiyanage88@users.noreply.github.com>
2025-08-30 22:16:18 +0530

    3 components done

--- constants/images.ts
+++ constants/images.ts
@@ -1,11 +1,31 @@
 import bg from "@/assets/images/bg.png";
+import cloud from "@/assets/images/cloud.png";
+import directionBg from "@/assets/images/direction-bg.png";
+import emptyStar from "@/assets/images/emptyStar.png";
+import { default as fullStar, default as heavyrain } from "@/assets/images/fullStar.png";
 import highlight from "@/assets/images/highlight.png";
+import line from "@/assets/images/line.png";
 import loginbg from "@/assets/images/login-bg.jpg";
+import mist from "@/assets/images/mist.png";
+import moderaterain from "@/assets/images/moderaterain.png";
+import partycloudy from "@/assets/images/partlycloudy.png";
 import rankingGradient from "@/assets/images/rankingGradient.png";
+import sun from "@/assets/images/sun.png";
+
 
 export const images = {
   bg,
   highlight,
   rankingGradient,
   loginbg,
+  cloud,
+  emptyStar,
+  fullStar,
+  heavyrain,
+  line,
+  mist,
+  moderaterain,
+  partycloudy,
+  sun,
+  directionBg
 };
