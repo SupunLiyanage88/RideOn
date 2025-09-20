799d8cfe2ffc5b16b4d5e3d7847ea733c59a73ab
Supun Liyanage <174965581+SupunLiyanage88@users.noreply.github.com>
2025-09-20 10:56:43 +0530

    feat: implement AddOrEditBikeDialog for bike management and update Admin component

--- app/(tabs)/admin.tsx
+++ app/(tabs)/admin.tsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import AddOrEditBikeDialog from "../admin/AddOrEditBikeDialog";
 import AddOrEditBikeStationDialog from "../admin/AddOrEditBikeStationDialog";
 
-
-
 const Admin = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+  const [bikeStationModalVisible, setBikeStationModalVisible] = useState(false);
+  const [bikeModalVisible, setBikeModalVisible] = useState(false);
 
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold mb-4">Admin</Text>
 
       <TouchableOpacity
-        onPress={() => setModalVisible(true)}
+        onPress={() => setBikeStationModalVisible(true)}
         className="bg-red-500 px-4 py-2 rounded-2xl"
       >
         <Text className="text-white font-semibold text-base">
@@ -21,9 +21,21 @@ const Admin = () => {
         </Text>
       </TouchableOpacity>
 
+      <TouchableOpacity
+        onPress={() => setBikeModalVisible(true)}
+        className="bg-red-500 px-4 py-2 rounded-2xl mt-5"
+      >
+        <Text className="text-white font-semibold text-base">Add a Bike</Text>
+      </TouchableOpacity>
+
       <AddOrEditBikeStationDialog
-        visible={modalVisible}
-        onClose={() => setModalVisible(false)}
+        visible={bikeStationModalVisible}
+        onClose={() => setBikeStationModalVisible(false)}
+      />
+
+      <AddOrEditBikeDialog
+        visible={bikeModalVisible}
+        onClose={() => setBikeModalVisible(false)}
       />
     </SafeAreaView>
   );
