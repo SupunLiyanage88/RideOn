ed7d620e662f61e10cb2b10d55d3a3a433cf9cb3
akilaManu <akilamanujith@gmail.com>
2025-10-01 12:26:32 +0530

    ddd

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Text, View } from "react-native";
+import AutoComplete from "../components/AutoComplete";
 
 const search = () => {
+  const sdgValueData = [
+    { label: "No Poverty", value: "1" },
+    { label: "Zero Hunger", value: "2" },
+    { label: "Good Health", value: "3" },
+    { label: "Quality Education", value: "4" },
+    { label: "Gender Equality", value: "5" },
+  ];
+  const { control, handleSubmit } = useForm({
+    defaultValues: { sdg: "" },
+  });
   return (
     <View>
       <Text>search</Text>
+      <View className="p-6">
+        <Controller
+          control={control}
+          name="sdg"
+          rules={{ required: "SDG is required" }}
+          render={({ field, fieldState: { error } }) => (
+            <AutoComplete
+              value={field.value}
+              onChange={field.onChange}
+              data={sdgValueData}
+              error={error?.message}
+              placeholder="Select Bike Type"
+              placeholderTextColor="red"
+            />
+          )}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default search
+export default search;
