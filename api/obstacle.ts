77b3d5c3c7959e94426b1886f776a97128f0bfdc
akilaManu <akilamanujith@gmail.com>
2025-10-11 03:58:55 +0530

    feat: Enhance obstacle management with hide functionality and location tracking

--- api/obstacle.ts
+++ api/obstacle.ts
@@ -17,11 +17,24 @@ export const obstacleSchema = z.object({
   category: z.enum(ObstacleCategory),
   isShow: z.boolean(),
   createdBy: userSchema,
+  obstacleLocation: z.number().optional(),
+  obstacleLongitude: z.number().optional(),
 });
 
 export type Obstacle = z.infer<typeof obstacleSchema>;
 
 export async function saveObstacle(data: Obstacle) {
   const res = await axios.post("/api/obstacle", data);
+  return res.data;
+}
+
+export async function hideObstacle(id: string) {
+  const res = await axios.patch(`/api/obstacle/${id}/is-show`);
+  return res.data;
+}
+
+export async function fetchObstacleData({ category }: { category: string }) {
+  console.log("Category in API:", category);
+  const res = await axios.get(`/api/obstacle?category=${category}`);
   return res.data;
 }
